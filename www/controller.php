fd29dd3733918979826185cec157257398275917
nm67 <nmoumoulides@gmail.com>
2013-06-28 18:50:31 +0100

    added REST-styled api backbone

---
+++ www/controller.php
@@ -0,0 +1,17 @@
+<?php
+include 'rest_api/rest_main';
+
+Controller::main();
+
+class Controller
+{
+	public static function main() {
+		// Call to REST api's "processRequest()"
+		
+		// Switch/If statement for HTTP methods 
+		// to perform the appropriate actions
+
+		
+	}
+}
+?>
