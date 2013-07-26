2a4e035702c143d4c80ab15078256bfeeec4cae5
nikos67 <nmoumoulides@gmail.com>
2013-07-26 21:56:09 +0100

    data_storage.php: Implemented JSON validation/parsing/appending algorithm throughout the data file instead of overwriting it...

--- www/data_manager/data_storage.php
+++ www/data_manager/data_storage.php
@@ -10,27 +10,100 @@ class DataStorage
 	value at the end of the object array, encodes the updated array into json,
 	and finally overwrites the file with the updated json-formatted data. */
 	public function storeData(array $valuesArray, $enoughSpace) {
+	
+		// **************************************** The file exists ****************************************
 		if(file_exists($this->JSONFileLocation)) {
-			$allDataJSON = file_get_contents($this->JSONFileLocation);
-			$allDataArray = json_decode($allDataJSON);
-			unset($allDataJSON); //prevent memory leaks for large json.
 
-			// We're out of disk space, remove the first data entry from the 
-			// stored JSON file, so that we can add a new one at the end.
-			if($enoughSpace == 0) {
-				array_shift($allDataArray);
+			// Open the file with pointer at Start of file.
+			$fHandle = fopen($this->JSONFileLocation, 'r+') or die("Can't open JSON file.\n");
+
+			// Read the whole thing.
+			$dataString = "";
+			while (!feof($fHandle)) {
+			   $dataString .= fgets($fHandle);
 			}
-			array_push($allDataArray, $valuesArray);
-			$updatedDataJSON = json_encode($allDataArray);
-			file_put_contents($this->JSONFileLocation, $updatedDataJSON);
-			echo json_encode($valuesArray);
-		}
+			
+			// Check if it is valid JSON by decoding it (the @ suppresses the error).
+			$phpArrayOfArrays = @json_decode($dataString);
+		
+			$noDataInFile = false;
+			
+			// ==================== The stored JSON file was Unsuccessfully decoded =========================
+			if($phpArrayOfArrays == false) {
+				echo "Bad-JSON Error: error when decoding the JSON file data...\n";
+				echo "Removing the last (invalid) JSON object.";
+				$posOfLastJSONObjectStart = strrpos($dataString,"{");
+				ftruncate($fHandle, $posOfLastJSONObjectStart-1);
+				
+				fseek($fHandle, 0, SEEK_END);
+				if(ftell($fHandle) < 2) {
+					$noDataInFile = true;
+				}
+				
+				// Try to encode the new values array into JSON (the @ suppresses the error).
+				$newJSONvalue = @json_encode($valuesArray);
+				
+				// ---------------- The new values array was unsuccessfully encoded into JSON... -------------
+				if($newJSONvalue == false) {
+					echo "Bad-JSON Error: error when encoding new values array to JSON...\n";
+					if($noDataInFile == true) {
+						fclose($fHandle);
+						unlink($this->JSONFileLocation);
+						echo "No data found in the file... Deleting the file...";
+					}
+					else {
+						fwrite($fHandle, "]");
+						fclose($fHandle);
+						echo "Closing the file...";
+					}
+				}
+				// ----------------- The new values array was successfully encoded into JSON... --------------
+				else {
+					if($noDataInFile == true) {
+						ftruncate($fHandle, 0);
+						fseek($fHandle, 0, SEEK_END);
+						$newJSONvalue = "[" . $newJSONvalue . "]";
+					}
+					else {
+						fseek($fHandle, 0, SEEK_END);
+						$newJSONvalue = "," . $newJSONvalue . "]";
+					}
+					fwrite($fHandle, $newJSONvalue);
+					fclose($fHandle);
+					echo "Storing new JSON object value...";
+				}
+			}
+			// ==================== The stored JSON file was successfully decoded =========================
+			else {
+				// Try to encode the new values array into JSON (the @ suppresses the error).
+				$newJSONvalue = @json_encode($valuesArray);
+				
+				// If encoding failed, don't write the value to file, just close it...
+				// ---------------- The new values array was unsuccessfully encoded into JSON... -------------
+				if($newJSONvalue == false) {
+						fclose($fHandle);
+						echo "Bad-JSON Error: error when encoding new values array to JSON...\n";
+						echo "Closing the file...";
+				}
+				// ----------------- The new values array was successfully encoded into JSON... --------------
+				else {
+					$posOfLastJSONObjectStart = strrpos($dataString,"]");
+					ftruncate($fHandle, $posOfLastJSONObjectStart);
+					fseek($fHandle, 0, SEEK_END);
+					$newJSONvalue = "," . $newJSONvalue . "]";
+					fwrite($fHandle, $newJSONvalue);
+					fclose($fHandle);
+					echo "Storing new JSON object value...";
+				}
+			}
+		} 
+		// ******************* No file exists -> Create it with the new data *******************
 		else { // Put the first array entry in the file.
 			$newAllDataArray = array ($valuesArray);
 			$tempJSONData = json_encode($newAllDataArray);
 			file_put_contents($this->JSONFileLocation, $tempJSONData);
 			echo "Created new storage file...\n";
-			echo "Started toring newly gathered data...";
+			echo "Started storing newly gathered data...";
 		}
 	}
 
